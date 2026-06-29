bdb1b5088b43998829c114112f676188a3778683
Ankit Yadav <nkityadav941318@gmail.com>
2026-06-29 14:28:23 +0530

    new server add

--- config/config.js
+++ config/config.js
@@ -2,29 +2,29 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT || "5433", 10),
+    username: "admin",
+    password: "password@Secret#2190",
+    database: "mehandigoapp",
+    host: "98.70.11.123",
+    port: 5432,
     dialect: "postgres",
   },
 
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT || "5433", 10),
+    username: "admin",
+    password: "password@Secret#2190",
+    database: "mehandigoapp",
+    host: "98.70.11.123",
+    port: 5432,
     dialect: "postgres",
   },
 
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT || "5433", 10),
+    username: "admin",
+    password: "password@Secret#2190",
+    database: "mehandigoapp",
+    host: "98.70.11.123",
+    port: 5432,
     dialect: "postgres",
   },
 };
